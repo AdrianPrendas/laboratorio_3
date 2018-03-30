b58bd7178fbf8f11cb796fa84143c2533ee7faea
_Adrian_Prendas_ <a6r1an@hotmail.com>
2018-03-30 12:50:09 -0600

    Los BL's faltantes

--- 03/app/src/main/java/com/example/a6r1an/lab03/domain/Grupo.kt
+++ 03/app/src/main/java/com/example/a6r1an/lab03/domain/Grupo.kt
@@ -1,14 +1,16 @@
 package com.example.a6r1an.lab03.domain
 
+import java.util.*
+
 /**
  * Created by Josue on 25/03/2018.
  */
 
 data class Grupo(
+        var nmeroGrupo: Int,
         var ciclo: Int,
         var curso: Int,
-        var nmeroGrupo: Int,
         var horario: String,
         var profesor: Int,
-        var estudiatesNotas: LinkedHashMap<Int,Int>
+        var estudiatesNotas: Hashtable<Int, Int>
 )
